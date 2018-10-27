ac279da9715ac67a96ce8d8c6393fe941d8feabe
rcullito <rob.culliton@gmail.com>
2018-10-27 15:49:39 +0200

    start removing packages we don't need to track

--- .emacs.d/elpa/solidity-mode-20171231.741/solidity-mode-autoloads.el
+++
@@ -1,29 +0,0 @@
-;;; solidity-mode-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "solidity-mode" "solidity-mode.el" (23176 51035
-;;;;;;  792711 298000))
-;;; Generated autoloads from solidity-mode.el
-
-(add-to-list 'auto-mode-alist '("\\.sol\\'" . solidity-mode))
-
-(autoload 'solidity-mode "solidity-mode" "\
-Major mode for editing solidity language buffers.
-
-\(fn)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("solidity-common.el" "solidity-flycheck.el"
-;;;;;;  "solidity-mode-pkg.el") (23176 51035 790724 394000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; solidity-mode-autoloads.el ends here
