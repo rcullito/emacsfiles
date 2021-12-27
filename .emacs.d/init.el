0a1c216e89f31eddaf528b382c3a04340be947f2
Rob Culliton <rob.culliton@gmail.com>
2021-12-27 12:19:55 -0500

    split out lsp into its own file. comment out typescript.el until the indentation is cleaned up and not affecting other modes

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -134,23 +134,8 @@
 (global-set-key (kbd "M-x") 'helm-M-x)
 
 
-(load-local "typescript.el")
-
-(straight-use-package 'lsp-mode)
-(straight-use-package 'lsp-treemacs)
-(add-hook 'clojure-mode-hook 'lsp)
-
-(setq gc-cons-threshold (* 100 1024 1024)
-      read-process-output-max (* 1024 1024)
-      treemacs-space-between-root-nodes nil
-      company-minimum-prefix-length 1
-      lsp-lens-enable t
-      lsp-signature-auto-activate nil
-      ; lsp-enable-indentation nil ; uncomment to use cider indentation instead of lsp
-      ; lsp-enable-completion-at-point nil ; uncomment to use cider completion instead of lsp
-      )
-
-(setq lsp-clojure-custom-server-command '("bash" "-c" "/usr/local/bin/clojure-lsp"))
+;; (load-local "typescript.el")
+(load-local "lsp.el")
 
 ;; beginning of assignment
 ;; setq-default will apply to all buffers
--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 ## TODO
 1. be able to center the narrowed region, currently it is stuck at the top of the buffer
 2. add font locking to deadgrep search results
+3. prevent typescript indentation from affecting other modes
 
 ## Help
 Packages that are not already installed will be installed on the first emacs run by straight
