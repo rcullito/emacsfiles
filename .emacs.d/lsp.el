a2bed3638a1f3fceae8bfca9d9160fb3218efccb
Rob Culliton <rob.culliton@gmail.com>
2021-12-27 12:42:44 -0500

    clean up along the lines of use-package config in lsp.el

--- .emacs.d/lsp.el
+++ .emacs.d/lsp.el
@@ -1,6 +1,8 @@
-(straight-use-package 'lsp-mode)
-(straight-use-package 'lsp-treemacs)
-(add-hook 'clojure-mode-hook 'lsp)
+
+(up lsp-mode
+    :config (add-hook 'clojure-mode-hook 'lsp))
+
+(up lsp-treemacs)
 
 (setq gc-cons-threshold (* 100 1024 1024)
       read-process-output-max (* 1024 1024)
@@ -8,8 +10,7 @@
       company-minimum-prefix-length 1
       lsp-lens-enable t
       lsp-signature-auto-activate nil
+      lsp-clojure-custom-server-command '("bash" "-c" "/usr/local/bin/clojure-lsp")
       ; lsp-enable-indentation nil ; uncomment to use cider indentation instead of lsp
       ; lsp-enable-completion-at-point nil ; uncomment to use cider completion instead of lsp
       )
-
-(setq lsp-clojure-custom-server-command '("bash" "-c" "/usr/local/bin/clojure-lsp"))
