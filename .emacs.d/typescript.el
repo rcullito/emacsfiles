f9eceed4d92f99febe945df0a4775837b0b81afc
Rob Culliton <rob.culliton@gmail.com>
2022-01-21 14:23:58 -0500

    remove unnecessary progn from use-package config

--- .emacs.d/typescript.el
+++ .emacs.d/typescript.el
@@ -1,15 +1,14 @@
 (require 'flycheck)
 
+(defun typescript-indentation ()
+  (setq typescript-indent-level 2)
+  (electric-indent-mode -1))
+
 (use-package typescript-mode
   :ensure t)
 
 (use-package add-node-modules-path
   :hook (typescript-mode web-mode))
-
-
-(defun typescript-indentation ()
-  (setq typescript-indent-level 2)
-  (electric-indent-mode -1))
 
 (use-package web-mode
     :init (progn
@@ -17,7 +16,8 @@
             (typescript-indentation))
     :ensure t
     :bind (:map web-mode-map ("TAB" . #'typescript-indent-line))
-    :config (flycheck-add-mode 'javascript-eslint 'web-mode))
+    :config 
+    (flycheck-add-mode 'javascript-eslint 'web-mode))
 
 (use-package tide
   :ensure t
@@ -26,14 +26,16 @@
   :hook ((typescript-mode . tide-setup)
          (web-mode . tide-setup)
          (typescript-mode . tide-hl-identifier-mode))
-  :config (progn
-            (flycheck-add-next-checker 'tsx-tide 'javascript-eslint)
-            (flycheck-add-next-checker 'typescript-tide 'javascript-eslint)))
+  :config 
+  (flycheck-add-next-checker 'tsx-tide 'javascript-eslint)
+  (flycheck-add-next-checker 'typescript-tide 'javascript-eslint))
+
+
 
 (use-package apheleia
   :straight (apheleia :host github :repo "raxod502/apheleia")
   :config
   (apheleia-global-mode t))
 
-;; (flycheck-add-next-checker 'typescript-tslint 'javascript-eslint)
+
 
